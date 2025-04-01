d1d5421f33569d70da5d85c3f82d12cc57eb58f5
ISAAC-MACIAS <isaac87@live.com.mx>
2025-04-01 17:56:57 -0600

    Add files via upload

---
+++ Voto.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Voto {
+    public static void main(String[] args) {
+        
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.print("Ingrese su edad: ");
+        int edad = scanner.nextInt();
+        
+        if (edad >= 18) {
+            System.out.println("Usted es elegible para votar.");
+        } else {
+            System.out.println("Usted no es elegible para votar.");
+        }
+
+        scanner.close();
+    }
+}
